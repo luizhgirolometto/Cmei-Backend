95624e263ddd49de31b9675a8e1b08705418a04c
Luiz <luizhgirolometto@hotmail.com>
2019-10-04 17:45:45 -0300

    TRADUÇÕES

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxx.firebaseapp.com",
-    databaseURL: "https://xxxxxxx.firebaseio.com",
-    projectId: "xxxxxxxx",
-    storageBucket: "xxxxxxxx.appspot.com",
-    messagingSenderId: "xxxxxxxxxxxxx",
-    appId: "1:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyAUzbuYa4cxbRlyyvSXlx1wJM9utW_w3XY",
+    authDomain: "quizcmei.firebaseapp.com",
+    databaseURL: "https://quizcmei.firebaseio.com",
+    projectId: "quizcmei",
+    storageBucket: "quizcmei.appspot.com",
+    messagingSenderId: "345127812711",
+    appId: "1:345127812711:web:cbf65f0aff2acb5811dec2",
+    measurementId: "G-VKW8JKT0KL"
   },
   defaultAnswerCount: 4, //Answer input count,
   snackBarConfig: {
